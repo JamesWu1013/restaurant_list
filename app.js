8a16135b48825051db2f030f7fdc60d003051f85
JamesWu1013 <a0988606683@gmail.com>
2022-12-20 17:02:57 +0800

    add create function

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-//const restaurants = require('./restaurants.json')
+const restaurants = require('./restaurants.json')
 const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 
 const mongoose = require('mongoose') // 載入 mongoose
 if (process.env.NODE_ENV !== 'production') {
@@ -26,6 +27,8 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -35,6 +38,38 @@ app.get('/', (req, res) => {
           .catch(error => console.log(error))
 
 })
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const id = req.body.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description     
+  return Restaurant.create({
+    id,
+    name,
+    name_en,
+    category,
+    image,
+    location,
+    phone,
+    google_map,
+    rating,
+    description
+   })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
 
 app.get('/restaurants/:restaurants_id', (req, res) => {
   console.log(req.params.restaurants_id)
