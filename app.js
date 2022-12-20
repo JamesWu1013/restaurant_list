8a8da478309e74cb75417cb68153f632647e6b9e
JamesWu1013 <a0988606683@gmail.com>
2022-12-20 17:45:51 +0800

    add detail page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurants = require('./restaurants.json')
+//const restaurants = require('./restaurants.json')
 const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 
@@ -71,21 +71,22 @@ app.post('/restaurants', (req, res) => {
 })
 
 
-app.get('/restaurants/:restaurants_id', (req, res) => {
-  console.log(req.params.restaurants_id)
-  const selectedRestaurant = restaurants.results.find((restaurant) => {
-    return restaurant.id.toString() === req.params.restaurants_id
-  })
-  res.render('show', { restaurant: selectedRestaurant })
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', {restaurant}))
+    .catch(error => console.log(error))
+
 })
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
-  const searchRestaurants = restaurants.results.filter((restaurant) => {
+  const searchRestaurants = Restaurant.results.filter((restaurant) => {
     return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
   })
-  res.render('index', { restaurants: searchRestaurants, keyword: keyword })
-})
+  res.render('index', { restaurant: searchRestaurants, keyword: keyword })
+})//先不管
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost: ${port}`)
